58223d066013481cfe5689b7924c233f364e2f2a
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-02-22 19:59:26 -0300

    Feat: Add sqlalchemy and database conection

--- project.py
+++ project.py
@@ -2,6 +2,17 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# DB Session maker 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+# Create session and connect to DB
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 @app.route('/')
 @app.route('/hello')
 def HelloWord():
