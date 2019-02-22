f678f53e93e2219d7681f60525bde0705d8730fd
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-02-22 20:24:25 -0300

    Feat: Learning about Routes

--- project.py
+++ project.py
@@ -16,9 +16,20 @@ session = DBSession()
 
 
 @app.route('/')
-@app.route('/hello')
-def HelloWord():
-    return "Hello World"
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+        output += i.price
+        output += '</br>'
+        output += i.description
+        output += '</br>'
+        output += '</br>'
+    return output
 
 
 if __name__ == '__main__':
